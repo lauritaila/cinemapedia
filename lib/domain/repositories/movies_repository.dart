d5a0c0e133e458ee0c6030f19ffb4fa316facf86
Laura Avila <lavila@bigwise.com>
2025-02-25 16:12:49 -0400

    FEAT: datasources, repositories, models and mappers implementation for movieDB now_playing

--- lib/domain/repositories/movies_repository.dart
+++ lib/domain/repositories/movies_repository.dart
@@ -1,6 +1,6 @@
 import 'package:cinemapedia/domain/entities/movie.dart';
 
-abstract class MoviesRepositories{
+abstract class MoviesRepository{
 
   Future<List<Movie>> getNowPlaying({int page = 1});
 
