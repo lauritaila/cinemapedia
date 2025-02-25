0bdff79258e6b6f5a3589439bb4fdb86c4afaa36
Laura Avila <lavila@bigwise.com>
2025-02-25 10:42:28 -0400

    FEAT: theme and router config, and first page

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,10 @@
+
+
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData getTheme() => ThemeData(
+      colorSchemeSeed: Color(0xFF2862F5),
+      useMaterial3: true,
+    );
+}
