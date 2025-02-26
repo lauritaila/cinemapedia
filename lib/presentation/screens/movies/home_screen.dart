b13554806c8e7f3733021d54e4694d2e1490fcb5
Laura Avila <lavila@bigwise.com>
2025-02-26 16:05:06 -0400

    FEAT: look movies on HomeScreen

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,6 +1,8 @@
 
 
+import 'package:cinemapedia/presentation/providers/movies/movies_providers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomeScreen extends StatelessWidget {
 
@@ -11,7 +13,36 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Placeholder(),
+      body: _HomeView(),
     );
+  }
+}
+
+class _HomeView extends ConsumerStatefulWidget {
+  const _HomeView();
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends ConsumerState<_HomeView> {
+
+  @override
+  void initState() {
+    super.initState();
+    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    
+    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+
+    if(nowPlayingMovies.isEmpty) return const Center(child: CircularProgressIndicator(strokeWidth: 2,));
+
+    return ListView.builder(
+      itemBuilder: (_, index) => ListTile(title: Text(nowPlayingMovies[index].title)), 
+      itemCount: nowPlayingMovies.length
+      );
   }
 }
