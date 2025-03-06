3564898d9fb133a104df5db662cdc289164fd899
Laura Avila <lavila@bigwise.com>
2025-03-06 11:30:03 -0400

    FIX: appbar vertical padding

--- lib/presentation/widgets/shared/custom_appbar.dart
+++ lib/presentation/widgets/shared/custom_appbar.dart
@@ -11,7 +11,7 @@ class CustomAppbar extends StatelessWidget {
     return SafeArea(
       bottom: false,
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10,vertical: BorderSide.strokeAlignCenter),
+        padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 10),
         child: SizedBox(
           width: double.infinity,
           child: Row(
