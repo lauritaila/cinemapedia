7762bed3232160a95ff45d5ae455b9f36352e64e
Laura Avila <lavila@bigwise.com>
2025-03-06 11:31:55 -0400

    FEAT: Slivers for homeScreen and popular, soon and best rating movies listview added

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -7,7 +6,6 @@ import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:cinemapedia/presentation/widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
-
   static const String name = 'home-screen';
 
   const HomeScreen({super.key});
@@ -16,8 +14,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: _HomeView(),
-      bottomNavigationBar: CustomBottomNavigation()
-      ,
+      bottomNavigationBar: CustomBottomNavigation(),
     );
   }
 }
@@ -30,7 +27,6 @@ class _HomeView extends ConsumerStatefulWidget {
 }
 
 class _HomeViewState extends ConsumerState<_HomeView> {
-
   @override
   void initState() {
     super.initState();
@@ -39,22 +35,70 @@ class _HomeViewState extends ConsumerState<_HomeView> {
 
   @override
   Widget build(BuildContext context) {
-
     final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
     final slideShowMovies = ref.watch(moviesSlideshowProvider);
 
     // if(nowPlayingMovies.isEmpty) return const Center(child: CircularProgressIndicator(strokeWidth: 2,));
 
-    return Column(
-      children: [
-        const CustomAppbar(),
-        MoviesSlideshow(movies: slideShowMovies),
-        MoviesHorizontalListview(
-          movies: nowPlayingMovies, 
-          label: 'In theaters', 
-          subTitle: 'Monday 20',
-          loadNextPage: () => ref.read(nowPlayingMoviesProvider.notifier).loadNextPage()
-        )
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          floating: true,
+          titleSpacing: 0,
+          flexibleSpace: FlexibleSpaceBar(
+            titlePadding: EdgeInsets.all(0),
+              centerTitle: false, title: CustomAppbar()),
+        ),
+        SliverList(
+          delegate: SliverChildBuilderDelegate(childCount: 1, (context, index) {
+            return Column(
+              children: [
+                SizedBox(height: 10),
+                MoviesSlideshow(movies: slideShowMovies),
+                MoviesHorizontalListview(
+                  movies: nowPlayingMovies,
+                  label: 'In theaters',
+                  subTitle: 'Monday 20',
+                  loadNextPage:
+                      () =>
+                          ref
+                              .read(nowPlayingMoviesProvider.notifier)
+                              .loadNextPage(),
+                ),
+                MoviesHorizontalListview(
+                  movies: nowPlayingMovies,
+                  label: 'Soon in theaters',
+                  // subTitle: '',
+                  loadNextPage:
+                      () =>
+                          ref
+                              .read(nowPlayingMoviesProvider.notifier)
+                              .loadNextPage(),
+                ),
+                MoviesHorizontalListview(
+                  movies: nowPlayingMovies,
+                  label: 'Popular',
+                  loadNextPage:
+                      () =>
+                          ref
+                              .read(nowPlayingMoviesProvider.notifier)
+                              .loadNextPage(),
+                ),
+                MoviesHorizontalListview(
+                  movies: nowPlayingMovies,
+                  label: 'Best rating',
+                  // subTitle: 'Monday 20',
+                  loadNextPage:
+                      () =>
+                          ref
+                              .read(nowPlayingMoviesProvider.notifier)
+                              .loadNextPage(),
+                ),
+                SizedBox(height: 20),
+              ],
+            );
+          }),
+        ),
       ],
     );
   }
