d7ac532fd887d58334325fb2e7716c4bf30841c4
Laura Avila <lavila@bigwise.com>
2025-03-07 16:58:05 -0400

    FEAT; implemented movie detail search

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -1,12 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:cinemapedia/presentation/providers/providers.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
 
-class MovieScreen extends StatelessWidget {
+class MovieScreen extends ConsumerStatefulWidget {
   static const name = 'movie-screen';
   final String movieId;
   const MovieScreen({super.key, required this.movieId});
 
   @override
+  MovieScreenState createState() => MovieScreenState();
+}
+
+class MovieScreenState extends ConsumerState<MovieScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    ref.read(movieDetailProvider.notifier).loadMovie(widget.movieId);
+  }
+
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('MovieId: $movieId')));
+
+    final Movie? movie = ref.watch(movieDetailProvider)[widget.movieId];
+
+    if(movie == null){
+      return Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(strokeWidth: 2),
+        ),
+      );
+    }
+
+    return Scaffold(body: Center(child: Text('MovieId: ${movie.title}')));
   }
 }
