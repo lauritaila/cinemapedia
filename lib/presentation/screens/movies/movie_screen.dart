5f72c6794f074ee216fe7534c1dfe897bb1bffdc
Laura Avila <lavila@bigwise.com>
2025-03-11 16:15:44 -0400

    FEAT: cast desing and implementation and image animation

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
@@ -17,6 +18,7 @@ class MovieScreenState extends ConsumerState<MovieScreen> {
   void initState() {
     super.initState();
     ref.read(movieDetailProvider.notifier).loadMovie(widget.movieId);
+    ref.read(castByMovieProvider.notifier).loadCast(widget.movieId);
   }
 
   @override
@@ -103,6 +105,7 @@ class _MovieDetails extends StatelessWidget {
             ],
           ),
         ),
+        _CastByMovie(movieId: movie.id.toString()),
         SizedBox(height: 50),
       ],
     );
@@ -131,7 +134,11 @@ class _CustomSliverAppBar extends StatelessWidget {
         background: Stack(
           children: [
             SizedBox.expand(
-              child: Image.network(movie.posterPath, fit: BoxFit.cover),
+              child: Image.network(movie.posterPath, fit: BoxFit.cover,
+              loadingBuilder:(context, child, loadingProgress){
+                if(loadingProgress != null) return const SizedBox();
+                return FadeIn(child: child);
+              }),
             ),
             // SizedBox.expand(
             //   child: DecoratedBox(
@@ -163,5 +170,55 @@ class _CustomSliverAppBar extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+
+class _CastByMovie extends ConsumerWidget {
+
+  final String movieId;
+  const _CastByMovie({required this.movieId});
+
+  @override
+  Widget build(BuildContext context, ref) {
+    final castByMovie = ref.watch(castByMovieProvider);
+
+    if(castByMovie[movieId] == null){
+      return SizedBox(
+        width: double.infinity,
+        height: 300,
+        child: Center(child: const CircularProgressIndicator(strokeWidth: 2)));
+    }
+
+    final casts = castByMovie[movieId]!;
+
+    return SizedBox(
+      height: 300,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: casts.length,
+        itemBuilder:(context, index){
+          final cast = casts[index];
+          return Container(
+            padding: const EdgeInsets.all(8),
+            width: 135,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                FadeInRight(
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child:  Image.network(cast.profilePath, height: 180,width: 135,fit: BoxFit.cover),
+                  ),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(cast.name, maxLines: 2),
+                Text(cast.character ?? '', maxLines: 2, style: TextStyle(fontWeight: FontWeight.bold, overflow: TextOverflow.ellipsis),)
+              ],
+            ),
+          );
+        } ));
   }
 }
