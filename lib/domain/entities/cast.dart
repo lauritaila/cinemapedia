a6bd433df70a34de4365531a4bdd2d108e468766
Laura Avila <lavila@bigwise.com>
2025-03-11 15:15:44 -0400

    FEAT: cast Entitie

---
+++ lib/domain/entities/cast.dart
@@ -0,0 +1,9 @@
+class Cast {
+  final int id;
+  final String name;
+  final String profilePath;
+  final String? character;
+
+  Cast({required this.id, required this.name, required this.profilePath, required this.character});
+
+}
