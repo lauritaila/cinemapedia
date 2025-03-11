aa7bdcc269a429c1d0301e0effcfed3caa4fcfed
Laura Avila <lavila@bigwise.com>
2025-03-11 15:16:08 -0400

    FEAT: Datasource getCastByMovie impl

---
+++ lib/infrastructure/datasources/moviedb_cast_datasources.dart
@@ -0,0 +1,27 @@
+import 'package:cinemapedia/config/constants/environment.dart';
+import 'package:cinemapedia/domain/datasources/cast_datasources.dart';
+import 'package:cinemapedia/domain/entities/cast.dart';
+import 'package:cinemapedia/infrastructure/mappers/cast_mapper.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/moviedb_credits.dart';
+import 'package:dio/dio.dart';
+
+class CastMovieDBDatasources extends CastDatasources{
+
+    final dio = Dio(BaseOptions(
+    baseUrl: 'https://api.themoviedb.org/3',
+    queryParameters: {
+      'api_key': Environment.theMovieDBApiKey,
+    }
+  ));
+
+
+  @override
+  Future<List<Cast>> getCastByMovie(String movieId) async {
+    final response = await dio.get('/movie/$movieId/credits');
+    if(response.statusCode != 200) throw Exception('Movie with id:$movieId not found');
+    final movieDbResponse = CreditsResponse.fromJson(response.data);
+    List<Cast> cast =  movieDbResponse.cast.map((cast)=> CastMapper.castToEntity(cast)).toList();
+    return cast;
+  }
+
+}
