fb8f77738e2576df52ed004aaa071616a7ff5167
Laura Avila <lavila@bigwise.com>
2025-03-19 16:32:54 -0400

    FEAT: add favorite button and separate gradient in othe widget for reuse

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -124,6 +124,13 @@ class _CustomSliverAppBar extends StatelessWidget {
       backgroundColor: Colors.black,
       expandedHeight: size.height * 0.7,
       foregroundColor: Colors.white,
+      actions: [
+        IconButton(
+          icon: const Icon(Icons.favorite_rounded, color: Colors.red),
+          // icon: const Icon(Icons.favorite_border_rounded),
+          onPressed: () {},
+        ),
+      ],
       flexibleSpace: FlexibleSpaceBar(
         // titlePadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
         // title: Text(
@@ -134,37 +141,32 @@ class _CustomSliverAppBar extends StatelessWidget {
         background: Stack(
           children: [
             SizedBox.expand(
-              child: Image.network(movie.posterPath, fit: BoxFit.cover,
-              loadingBuilder:(context, child, loadingProgress){
-                if(loadingProgress != null) return const SizedBox();
-                return FadeIn(child: child);
-              }),
-            ),
-            // SizedBox.expand(
-            //   child: DecoratedBox(
-            //     decoration: BoxDecoration(
-            //       gradient: LinearGradient(
-            //         begin: Alignment.topCenter,
-            //         end: Alignment.bottomCenter,
-            //         stops: [0.7,1.0],
-            //         colors: [
-            //         Colors.transparent,
-            //         Colors.black87
-            //       ]
-            //     )),
-            //   ),
-            // ),
-            SizedBox.expand(
-              child: DecoratedBox(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomCenter,
-                    stops: [0.0, 0.3],
-                    colors: [Colors.black87, Colors.transparent],
-                  ),
-                ),
+              child: Image.network(
+                movie.posterPath,
+                fit: BoxFit.cover,
+                loadingBuilder: (context, child, loadingProgress) {
+                  if (loadingProgress != null) return const SizedBox();
+                  return FadeIn(child: child);
+                },
               ),
+            ),
+            _CustomGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Colors.transparent, Colors.black54],
+              stops: [0.7, 1.0],
+            ),
+            _CustomGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [Colors.black54, Colors.transparent],
+              stops: [0.0, 0.2],
+            ),
+            _CustomGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomCenter,
+              colors: [Colors.black87, Colors.transparent],
+              stops: [0.0, 0.3],
             ),
           ],
         ),
@@ -173,9 +175,36 @@ class _CustomSliverAppBar extends StatelessWidget {
   }
 }
 
+class _CustomGradient extends StatelessWidget {
+  final AlignmentGeometry begin;
+  final AlignmentGeometry end;
+  final List<Color> colors;
+  final List<double> stops;
+  const _CustomGradient({
+    required this.begin,
+    required this.end,
+    required this.colors,
+    required this.stops,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox.expand(
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: begin,
+            end: end,
+            stops: stops,
+            colors: colors,
+          ),
+        ),
+      ),
+    );
+  }
+}
 
 class _CastByMovie extends ConsumerWidget {
-
   final String movieId;
   const _CastByMovie({required this.movieId});
 
@@ -183,11 +212,12 @@ class _CastByMovie extends ConsumerWidget {
   Widget build(BuildContext context, ref) {
     final castByMovie = ref.watch(castByMovieProvider);
 
-    if(castByMovie[movieId] == null){
+    if (castByMovie[movieId] == null) {
       return SizedBox(
         width: double.infinity,
         height: 300,
-        child: Center(child: const CircularProgressIndicator(strokeWidth: 2)));
+        child: Center(child: const CircularProgressIndicator(strokeWidth: 2)),
+      );
     }
 
     final casts = castByMovie[movieId]!;
@@ -197,7 +227,7 @@ class _CastByMovie extends ConsumerWidget {
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
         itemCount: casts.length,
-        itemBuilder:(context, index){
+        itemBuilder: (context, index) {
           final cast = casts[index];
           return Container(
             padding: const EdgeInsets.all(8),
@@ -208,17 +238,29 @@ class _CastByMovie extends ConsumerWidget {
                 FadeInRight(
                   child: ClipRRect(
                     borderRadius: BorderRadius.circular(20),
-                    child:  Image.network(cast.profilePath, height: 180,width: 135,fit: BoxFit.cover),
+                    child: Image.network(
+                      cast.profilePath,
+                      height: 180,
+                      width: 135,
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
-                SizedBox(
-                  height: 5,
-                ),
+                SizedBox(height: 5),
                 Text(cast.name, maxLines: 2),
-                Text(cast.character ?? '', maxLines: 2, style: TextStyle(fontWeight: FontWeight.bold, overflow: TextOverflow.ellipsis),)
+                Text(
+                  cast.character ?? '',
+                  maxLines: 2,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
               ],
             ),
           );
-        } ));
+        },
+      ),
+    );
   }
 }
