7abdf3ab89f9c3e56037ff94e617ac1d50ba049c
Laura Avila <lavila@bigwise.com>
2025-03-21 15:44:57 -0400

    FEAT: masonry widgets

---
+++ lib/presentation/widgets/movies/movie_masonry.dart
@@ -0,0 +1,42 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/presentation/widgets/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
+class MovieMasonry extends StatefulWidget {
+
+  final List<Movie> movies;
+  final VoidCallback? loadNextPage;
+
+  const MovieMasonry({super.key, required this.movies, this.loadNextPage});
+
+  @override
+  State<MovieMasonry> createState() => _MovieMasonryState();
+}
+
+class _MovieMasonryState extends State<MovieMasonry> {
+  //TODO initstate
+  //TODO dispose
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: MasonryGridView.count(
+        crossAxisCount: 3,
+        mainAxisSpacing: 10,
+        crossAxisSpacing: 10,
+        itemCount: widget.movies.length,
+        itemBuilder: (context, index){
+          if (index == 1){
+            return Column(
+              children: [
+                const SizedBox(height: 30,),
+                MoviePosterLink(movie: widget.movies[index]),
+              ],
+            );
+          }
+          return MoviePosterLink(movie: widget.movies[index]);
+        }),
+    );
+  }
+}
