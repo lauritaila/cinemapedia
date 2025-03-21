c898acf1af538e4ff6a390c5c3e3b3550877943f
Laura Avila <lavila@bigwise.com>
2025-03-21 15:45:22 -0400

    FEAT: updated favory view to view masonry

--- lib/presentation/views/movies/favorites_view.dart
+++ lib/presentation/views/movies/favorites_view.dart
@@ -1,12 +1,30 @@
+import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/presentation/providers/providers.dart';
 
-class FavoritesView extends StatelessWidget {
+class FavoritesView extends ConsumerStatefulWidget {
   const FavoritesView({super.key});
 
   @override
+  FavoritesViewState createState() => FavoritesViewState();
+}
+
+class FavoritesViewState extends ConsumerState<FavoritesView> {
+
+  @override
+  void initState() async {
+    super.initState();
+    ref.read(favoriteMovieProvider.notifier).loadNextPage();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final List<Movie> movies = ref.watch(favoriteMovieProvider).values.toList();
+
     return Scaffold(
-      appBar: AppBar(title: const Text('Favorites')),
-      body: Center(child: Text('Favorites View')));
+      body: MovieMasonry(movies: movies)
+      );
   }
 }
