d132da3f944f541a02c267dd4df6e8ee0e59fd25
Laura Avila <lavila@bigwise.com>
2025-03-24 16:55:49 -0400

    FEAT: change import

--- lib/presentation/providers/storage/local_storage_repository_provider.dart
+++ lib/presentation/providers/storage/local_storage_repository_provider.dart
@@ -1,7 +1,7 @@
 
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:cinemapedia/domain/repositories/local_storage_repository_impl.dart';
-import 'package:cinemapedia/domain/datasources/isar_datasource.dart';
+import 'package:cinemapedia/infrastructure/repositories/local_storage_repository_impl.dart';
+import 'package:cinemapedia/infrastructure/datasources/isar_datasource.dart';
 
 final localStorageRepositoryProvider = Provider((ref) => LocalStorageRepositoryImpl(IsarDatasource()));
