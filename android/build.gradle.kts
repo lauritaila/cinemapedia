fbbaa6b79a61057b651869bbe75569df2a7d2318
Laura Avila <lauritaila10gmail.com>
2025-03-26 21:07:13 -0400

    FIX: namespace isar bug

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,7 +1,23 @@
+import com.android.build.api.dsl.LibraryExtension
+import com.android.build.api.dsl.ApplicationExtension
+
 allprojects {
     repositories {
         google()
         mavenCentral()
+    }
+}
+
+subprojects {
+    afterEvaluate {
+        when (val ext = project.extensions.findByName("android")) {
+            is ApplicationExtension -> {
+                ext.namespace = "com.example.cinemapedia" // Usa tu package name
+            }
+            is LibraryExtension -> {
+                ext.namespace = project.group?.toString() ?: "dev.isar.isar_flutter_libs"
+            }
+        }
     }
 }
 
